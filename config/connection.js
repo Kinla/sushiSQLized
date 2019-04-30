d26521b5d4283683e91415a9ee36206d819f8b6d
SharonC <sharon.s.c.chien@gmail.com>
2019-04-29 23:27:07 -0400

    setting up JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,23 @@
 const mysql = require("mysql");
+const connection;
 
-const connection = mysql.createConnection({
-  host: "localhost",
-
-  // Your port; if not 3306
-  port: 3306,
-
-  // Your username
-  user: "root",
-
-  // Your password
-  password: "root",
-  database: "sushi_db"
-});
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "root",
+    database: "sushi_db"
+  });
+}
 
 connection.connect();
 
--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@ USE sushi_db;
 -- If the table already exists, remove it before trying to create the table again
 DROP TABLE IF EXISTS sushi;
 
--- Create the burgers table
+-- Create the sushi table
 CREATE TABLE sushi (
     id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     sushi_name VARCHAR(300) NOT NULL,
