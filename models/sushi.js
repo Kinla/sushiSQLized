33ff1d5b1c1af4664f8427bcc8df265ece3e48fd
SharonC <sharon.s.c.chien@gmail.com>
2019-05-02 01:20:33 -0400

    changed model but validation error doesn't result in reload

--- models/sushi.js
+++ models/sushi.js
@@ -3,8 +3,17 @@ const Sequelize = require("sequelize")
 const sequelize = require("../config/connection.js")
 
 let Sushi = sequelize.define("sushi", {
-    sushi_name: Sequelize.STRING,
-    devoured: Sequelize.BOOLEAN
+    sushi_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            len: [1,50]
+        }
+    },
+    devoured: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+    }
 })
 
 Sushi.sync()
